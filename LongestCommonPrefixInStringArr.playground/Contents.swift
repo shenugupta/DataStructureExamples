a472b17798710cd5c82992693b25c950aed78d45
Shenu Gupta <>
2021-06-24 10:37:11 +0530

    comments added in LCP program

--- LongestCommonPrefixInStringArr.playground/Contents.swift
+++ LongestCommonPrefixInStringArr.playground/Contents.swift
@@ -6,20 +6,24 @@ struct LongestCommonPrefix {
     
     func getLongestCommonPrefixInGivenArray(arr: [String]) -> String {
         var lcp = arr[0]
+        // get the first element of array
         let lcpCharacters = Array(lcp)
         var currentWord = ""
+        // iterate over starting second element to end of the element in the array
         for i in 1...arr.count - 1 {
+            // get the current word in the array
             currentWord = arr[i]
             let characters = Array(currentWord)
             var j = 0
-          
+          // check for the j index and its length should be less than to current word lenghth and lcp length and check if sequence of characters matching to current word array in the string
             while j<currentWord.count && j < lcp.count && lcpCharacters[j] == characters[j] {
                 j = j + 1
-                
             }
+            // if no character matching in current word and lcp word then return 0
             if j == 0 {
                 return ""
             }
+            // extract substring that matching characters from lcp word and current word in the array and return this string
             lcp = String(characters[0..<j])
         }
        return lcp
