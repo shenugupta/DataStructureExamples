f4c8563edf00d8d00b8037739411d4cb6fcf024e
Shenu Gupta <>
2021-06-29 21:39:53 +0530

    changes in FirstAndLastOccuranceFile

--- FirstAndLastOccurance.playground/Contents.swift
+++ FirstAndLastOccurance.playground/Contents.swift
@@ -6,30 +6,37 @@ func getIndex(arr: [Int], targetVal: Int, getFirst: Bool) -> Int {
     var start = 0
     var end = arr.count - 1
     var ans: Int = 0
-    while start<end {
+    while start<=end {
+        // get mid position in the arrat
         let mid = start + (end - start)/2
+        // check if the target value matches in the array
         if arr[mid] == targetVal {
             ans = mid
+            // fetch the first occurance in the array
             if getFirst {
               end = mid - 1
             }
+            // fetch the last occurance in the array
             else {
              start =  mid + 1
             }
-        } else if  arr[mid] < targetVal{
+        }
+        // if target value greater than array mid then search right side of the array
+        else if  arr[mid] < targetVal{
             start =  mid + 1
         }
+        // if target value less than array mid then search left side of the array
         else {
             end = mid - 1
         }
     }
     return ans
-}
+ }
 }
 
 var sortedArray = [1,1,2,2,3,3,4,4,4,5]
 let firstAndLastOcc = FirstAndLastOccurance()
-var getFirstIndex = firstAndLastOcc.getIndex(arr: sortedArray, targetVal: 3, getFirst: true)
-var getLastIndex = firstAndLastOcc.getIndex(arr: sortedArray, targetVal: 3, getFirst: false)
+var getFirstIndex = firstAndLastOcc.getIndex(arr: sortedArray, targetVal: 1, getFirst: true)
+var getLastIndex = firstAndLastOcc.getIndex(arr: sortedArray, targetVal: 1, getFirst: false)
 print(getFirstIndex)
-print(getLastIndexh)
+print(getLastIndex)
